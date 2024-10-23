eda298649ef2ecdf84a08b53d3ee83ffa838f4bb
bladidev <bladi.rp02@gmail.com>
2024-10-23 12:25:49 -0600

    change name to user

---
+++ apps/users/src/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './users/user.controller';
+import { UserService } from './users/user.service';
+
+@Module({
+  imports: [],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
