bf980616dfac691d95dc822d5cf6c6414114461e
bladidev <bladi.rp02@gmail.com>
2024-10-24 08:45:55 -0600

    add user interface in shared

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,3 +1,4 @@
 export * from './shared.module';
 export * from './shared.service';
 export * from './constants/proxy.constants'
+export * from './interfaces/interface.index';
---
+++ libs/shared/src/interfaces/interface.index.ts
@@ -0,0 +1,2 @@
+export * from './ms-task/i-task.interface';
+export * from './ms-user/i-user.interface';
